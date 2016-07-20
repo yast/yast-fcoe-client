1df5f0ab68718ddfa9ff06343e0f292f1e1e4a24
Gabriele Mohr <gs@suse.de>
2016-07-20 10:43:19 +0200

    enhance test data (same VLAN ID and "0" VLAN)

--- test/GetVlanInterfaces_spec.rb
+++ test/GetVlanInterfaces_spec.rb
@@ -8,21 +8,57 @@ describe Yast::FcoeClientClass do
   end
   
   describe "#GetVlanInterfaces" do
-    context "with valid arguments" do
-      it "returns map containing info about vlan interfaces per netcard" do
+    context "with a list of netcards and a list of corresponding VLANs as arguments" do
+      it "returns info about VLAN interfaces per netcard (without identical VLAN IDs)" do
         expect(@fcoe.GetVlanInterfaces(["eth0", "eth1", "eth2"],
                                        ["eth0     | 200  | 00:0d:ec:a2:ef:00",
                                         "eth0     | 300  | 00:0d:ec:a2:ef:01",
-                                        "eth2     | 200  | 00:0d:ec:a2:ef:02" ])).to eq(
+                                        "eth0     | 300  | 00:0d:ec:a2:ef:02",
+                                        "eth1     | 400  | 00:ef:af:34:12:ae",
+                                        "eth1     | 400  | 00:ef:af:34:12:af",
+                                        "eth2     | 200  | 00:0d:ec:a2:ef:03" ])).to eq(
                       {"eth0" => [{ "vlan" => "200", "fcf" => "00:0d:ec:a2:ef:00" },
                                   { "vlan" => "300", "fcf" => "00:0d:ec:a2:ef:01" }],
-                       "eth2" => [{ "vlan" => "200", "fcf" => "00:0d:ec:a2:ef:02" }]
+                       "eth1" => [{ "vlan" => "400", "fcf" => "00:ef:af:34:12:ae" }],
+                       "eth2" => [{ "vlan" => "200", "fcf" => "00:0d:ec:a2:ef:03" }]
                       })
       end
     end
   end
+
   describe "#GetVlanInterfaces" do
-    context "with an empty list as argument" do
+    context "with a list of netcards and a list of VLANs including '0' VLAN as arguments" do
+      it "returns info about VLAN interfaces per netcard (only '0' VLAN for identical FCFs)" do
+        expect(@fcoe.GetVlanInterfaces(["eth0", "eth1", "eth2"],
+                                       ["eth0     | 200  | 00:0d:ec:a2:ef:00",
+                                        "eth0     | 300  | 00:0d:ec:a2:ef:01",
+                                        "eth1     | 2016 | 00:ef:af:34:12:ae",
+                                        "eth1     | 0    | 00:ef:af:34:12:ae",
+                                        "eth2     | 200  | 00:0d:ec:a2:ef:03" ])).to eq(
+                      {"eth0" => [{ "vlan" => "200", "fcf" => "00:0d:ec:a2:ef:00" },
+                                  { "vlan" => "300", "fcf" => "00:0d:ec:a2:ef:01" }],
+                       "eth1" => [{ "vlan" => "0", "fcf" => "00:ef:af:34:12:ae" }],
+                       "eth2" => [{ "vlan" => "200", "fcf" => "00:0d:ec:a2:ef:03" }]
+                      })
+      end
+    end
+  end
+
+  describe "#GetVlanInterfaces" do
+    context "with a list of netcards and a list of VLANs but not for every netcard" do
+      it "returns a map containing info about VLAN interfaces only for netcard having VLANs" do
+        expect(@fcoe.GetVlanInterfaces(["eth0", "eth1", "eth2"],
+                                       ["eth0     | 200  | 00:0d:ec:a2:ef:00",
+                                        "eth0     | 300  | 00:0d:ec:a2:ef:01" ])).to eq(
+                      {"eth0" => [{ "vlan" => "200", "fcf" => "00:0d:ec:a2:ef:00" },
+                                  { "vlan" => "300", "fcf" => "00:0d:ec:a2:ef:01" }]
+                      })
+      end
+    end
+  end
+
+  describe "#GetVlanInterfaces" do
+    context "with an empty list of VLANs as argument" do
       it "returns an empty map" do
         expect(@fcoe.GetVlanInterfaces(["eth0", "eth1", "eth2"], [])).to eq({})
       end
