c3365a9c689c8b4dc9cbd731d31c72c3c62ab263
Ladislav Slezák <lslezak@suse.cz>
2020-12-08 18:04:45 +0100

    Fixed unit tests - use new RSpec syntax

--- test/DetectNetworkCards_spec.rb
+++ test/DetectNetworkCards_spec.rb
@@ -3,37 +3,34 @@ require_relative "test_helper"
 Yast.import "FcoeClient"
 
 describe Yast::FcoeClientClass do
+  subject { Yast::FcoeClient }
+
   before :each do
-    @fcoe = Yast::FcoeClientClass.new
-    @fcoe.main()
-
-    @test_class = @fcoe
-
-    @test_class.stub(:GetVlanInterfaces).and_return({ "eth1" => [{ "vlan" => "400", "fcf" => "00:0d:ec:a2:ef:00" },
+    allow(subject).to receive(:GetVlanInterfaces).and_return({ "eth1" => [{ "vlan" => "400", "fcf" => "00:0d:ec:a2:ef:00" },
                                                                  { "vlan" => "300", "fcf" => "00:0d:ec:a2:ef:01" }],
                                                       "eth2" => [{ "vlan" => "200", "fcf" => "00:0d:ec:a2:ef:02" }]})
 
-    @test_class.stub(:GetFcoeVlanInterface).with("eth1", "400").and_return("eth1.400")
-    @test_class.stub(:GetFcoeVlanInterface).with("eth1", "300").and_return("")
-    @test_class.stub(:GetFcoeVlanInterface).with("eth2", "200").and_return("")
+    allow(subject).to receive(:GetFcoeVlanInterface).with("eth1", "400").and_return("eth1.400")
+    allow(subject).to receive(:GetFcoeVlanInterface).with("eth1", "300").and_return("")
+    allow(subject).to receive(:GetFcoeVlanInterface).with("eth2", "200").and_return("")
 
-    @test_class.stub(:FcoeOnOnterface?).and_return(false)
+    allow(subject).to receive(:FcoeOnInterface?).and_return(false)
 
-    @test_class.stub(:GetFcoeStatus).with("eth1.400", "eth1").and_return({
+    allow(subject).to receive(:GetFcoeStatus).with("eth1.400", "eth1").and_return({
                                                                            "AUTO_VLAN" => "no",
                                                                            "DCB_REQUIRED"=>"yes",
                                                                            "FCOE_ENABLE" => "yes",
                                                                            "cfg_device" => "eth1.400"
                                                                          })
-    @test_class.stub(:GetFcoeStatus).with("eth1.300", "eth1").and_return({})
-    @test_class.stub(:GetFcoeStatus).with("eth2.200", "eth2").and_return({})
+    allow(subject).to receive(:GetFcoeStatus).with("eth1.300", "eth1").and_return({})
+    allow(subject).to receive(:GetFcoeStatus).with("eth2.200", "eth2").and_return({})
 
-    @test_class.stub(:DCBCapable).and_return("no")
+    allow(subject).to receive(:DCBCapable).and_return("no")
   end
 
   context "with valid argument (a list of maps containing hwinfo about netcards)" do
     it "#DetectNetworkCards returns a list of maps containing card info extended by vlan/fcoe info" do
-      answer = @test_class.DetectNetworkCards([
+      answer = subject.DetectNetworkCards([
                                                {
                                                  "bus"       => "PCI",
                                                  "bus_hwcfg" => "pci",
@@ -59,7 +56,7 @@ describe Yast::FcoeClientClass do
                                                  "iscsioffload"=> false
                                                }
                                               ])
-      answer.should eq([
+      expect(answer). to eq([
                         {
                           "auto_vlan" => "yes", 
                           "cfg_device"=> "", 
@@ -109,8 +106,7 @@ describe Yast::FcoeClientClass do
 
   context "with empty list as argument" do
     it "#DetectNetworkCards returns empty list" do
-      answer = @test_class.DetectNetworkCards([])
-      answer.should eq([])
+      expect(subject.DetectNetworkCards([])).to eq([])
     end
   end
 
